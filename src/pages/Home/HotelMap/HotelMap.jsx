4df187f62f8284411e16580959cee61ff18be6d2
thkiaAdiba <athkiaadiba@gmail.com>
2024-05-13 22:34:32 +0600

    added map icon

--- src/pages/Home/HotelMap/HotelMap.jsx
+++ src/pages/Home/HotelMap/HotelMap.jsx
@@ -1,20 +1,27 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import mapIcon from '../../../assets/location_icon.png';
+
+const customIcon = L.icon({
+    iconUrl: mapIcon, 
+    iconSize: [50, 50], 
+    iconAnchor: [16, 32], 
+    popupAnchor: [0, -32] 
+});
 
 
 const HotelMap = () => {
     const position = [23.797911, 90.414391]
     return (
-        <MapContainer 
-        center={position} zoom={13} scrollWheelZoom={false} style={{ height: "400px", width: "100%", zIndex: "0" }}>
+        <MapContainer
+            center={position} zoom={13} scrollWheelZoom={false} style={{ height: "400px", width: "100%", zIndex: "0" }}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={position}>
-                <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
+            <Marker position={position} icon={customIcon}>
+                
             </Marker>
         </MapContainer>
     );
